042c2c2fa7abd9c74fd1a1fc7f83c87722fb8f33
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-08-11 11:02:51 +0600

    first commit

---
+++ problem1.js
@@ -0,0 +1,21 @@
+function matchWinner(teamAGoals, teamBGoals) {
+    // console.log(typeof teamAGoals, typeof teamBGoals);
+    if((typeof teamAGoals==="number") && (typeof teamBGoals==="number") ){
+      if(teamAGoals>teamBGoals){
+        return "Team A Won";
+      }
+      else if(teamAGoals<teamBGoals){
+        return "Team B Won";
+      }
+      else{
+        return "Draw";
+      }
+  }
+  else{
+    return "Invalid";
+  }
+}
+console.log(matchWinner(2,1))
+console.log(matchWinner(1,3))
+console.log(matchWinner(2,2))
+console.log(matchWinner("3",2))
